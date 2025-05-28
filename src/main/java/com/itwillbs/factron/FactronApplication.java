7fc1667b0c844ca0a0d6999c33f197cdb2ea3e0a
chocoham <banma1234@gmail.com>
2025-05-28 10:07:31 +0900

    build: spring boot 초기 빌드 파일

---
+++ src/main/java/com/itwillbs/factron/FactronApplication.java
@@ -0,0 +1,13 @@
+package com.itwillbs.factron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itwillbs/factron/FactronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itwillbs.factron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
