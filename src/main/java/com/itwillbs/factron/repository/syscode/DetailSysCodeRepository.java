e293fc921c8742425a2733142357039602cc4de4
CANADAGAGOPA <kswms40335@gmail.com>
2025-06-05 19:06:19 +0900

    fix: JpaRepository PK 타입 수정

--- src/main/java/com/itwillbs/factron/repository/syscode/DetailSysCodeRepository.java
+++ src/main/java/com/itwillbs/factron/repository/syscode/DetailSysCodeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DetailSysCodeRepository extends JpaRepository<DetailSysCode, Integer> {
+public interface DetailSysCodeRepository extends JpaRepository<DetailSysCode, Long> {
 }
--- src/main/java/com/itwillbs/factron/repository/transfer/TransferRepository.java
+++ src/main/java/com/itwillbs/factron/repository/transfer/TransferRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransferRepository extends JpaRepository<Transfer, Integer> {
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
 }
