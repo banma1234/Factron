7390a070522cd542a1df754988b6bd0e3e45c736
Pangwoo <gwangwoo25@gmail.com>
2025-06-08 19:13:01 +0900

    feat: 백앤드 사원 추가

---
+++ src/main/java/com/itwillbs/factron/common/component/AESUtil.java
@@ -0,0 +1,66 @@
+package com.itwillbs.factron.common.component;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+@Component
+@Log4j2
+public class AESUtil {
+
+    @Value("${aes.secret-key}")
+    private String secretKey;
+
+    public String encrypt(String plainText) throws Exception {
+        try {
+            log.info("secretKey: " + secretKey);
+            log.info("plainText: " + plainText);
+            SecretKeySpec key = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] encrypted = cipher.doFinal(plainText.getBytes());
+
+            return Base64.getEncoder().encodeToString(encrypted);
+        } catch (Exception e) {
+            throw new RuntimeException("AES 암호화 실패", e);
+        }
+    }
+
+
+    public String decrypt(String encryptedText) throws Exception {
+        try {
+            log.info("secretKey: " + secretKey);
+            log.info("encryptedText: " + encryptedText);
+            SecretKeySpec key = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decoded = Base64.getDecoder().decode(encryptedText);
+
+            return new String(cipher.doFinal(decoded), StandardCharsets.UTF_8);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException |
+                 IllegalBlockSizeException | BadPaddingException e) {
+            // 로그 출력
+            log.error("AES 복호화 실패", e);
+            // 필요하면 커스텀 런타임 예외 던지기
+            throw new RuntimeException("복호화 과정에서 오류가 발생했습니다.");
+        }
+
+
+    }
+
+
+
+
+}
--- src/main/java/com/itwillbs/factron/dto/employee/RequestEmployeeNewDTO.java
+++ src/main/java/com/itwillbs/factron/dto/employee/RequestEmployeeNewDTO.java
@@ -1,16 +1,20 @@
 package com.itwillbs.factron.dto.employee;
 
 import com.itwillbs.factron.entity.Employee;
+import com.itwillbs.factron.entity.IntergratAuth;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RequestEmployeeNewDTO {
     private String empName; // 사원 이름
-    private String residentRegistrationNumber; // 주민번호
+    private String birth; // 주민번호
+    private String rrnBack; // 주민번호
     private String email; // 이메일
     private String gender; // 성별
     private String phone; // 전화번호
@@ -19,11 +23,34 @@ public class RequestEmployeeNewDTO {
     private String eduLevelCode; // 학력 코드
     private String positionCode; // 직급 코드
     private String deptCode; // 부서 코드
-    private String employeCode; //입사 유형 코드
+    private String employCode; //입사 유형 코드
+    private String isActive; // 재직 상태
 
-    public Employee toEntity() {
-        return Employee.builder().
+    public Employee toEntity(Long newId, Long addBy) {
+        return Employee.builder()
+                .id(newId)
+                .employCode(employCode)
+                .deptCode(deptCode)
+                .positionCode(positionCode)
+                .eduLevelCode(eduLevelCode)
+                .name(empName)
+                .address(address)
+                .email(email)
+                .phone(phone)
+                .birth(birth)
+                .rrnBack(rrnBack)
+                .gender(gender)
+                .joinedDate(joinedDate != null && !joinedDate.isBlank() ? LocalDate.parse(joinedDate) : null)
+                .createdBy(addBy)
+                .build();
+    }
 
-            .build();
+    public IntergratAuth toIntergratAuth(Employee employee) {
+        return IntergratAuth.builder()
+                .password(phone)
+                .authCode(positionCode)
+                .isActive(isActive)
+                .employee(employee)
+                .build();
     }
 }
