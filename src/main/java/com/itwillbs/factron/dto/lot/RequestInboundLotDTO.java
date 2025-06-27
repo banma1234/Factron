66d80123afc94e1c8edd57e154d451c4f39eaa10
chocoham <banma1234@gmail.com>
2025-06-27 13:56:09 +0900

    refactor: LotType 및 requestDTO 속성 변경, 주석 추가

--- src/main/java/com/itwillbs/factron/dto/lot/RequestInboundLotDTO.java
+++ src/main/java/com/itwillbs/factron/dto/lot/RequestInboundLotDTO.java
@@ -1,17 +1,20 @@
 package com.itwillbs.factron.dto.lot;
 
+import com.itwillbs.factron.entity.Item;
 import com.itwillbs.factron.entity.Lot;
 import com.itwillbs.factron.entity.Material;
 import com.itwillbs.factron.entity.enums.LotType;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+/**
+ * (입고) LOT생성 요청 DTO
+ * */
 @Data
 public class RequestInboundLotDTO {
 
-    @NotBlank(message = "자재는 필수 입력값입니다.")
     private Material material;
+    private Item item;
 
     @NotNull(message = "수량은 필수 입력값입니다.")
     private Long quantity;
@@ -19,16 +22,20 @@ public class RequestInboundLotDTO {
     @NotNull(message = "유형 구분은 필수 입력값입니다.")
     private LotType event_type;
 
-    public RequestInboundLotDTO(Material material, Long quantity, LotType event_type) {
+    public RequestInboundLotDTO(Material material, Item item, Long quantity, LotType event_type) {
         this.material = material;
+        this.item = item;
         this.quantity = quantity;
         this.event_type = event_type;
     }
 
+    /**
+     * DTO => Entity 변환 메서드
+     * */
     public Lot toEntity(String LotId) {
         return Lot.builder()
                 .id(LotId)
-                .item(null)
+                .item(this.item)
                 .material(this.material)
                 .quantity(this.quantity)
                 .eventType(this.event_type.getPrefix())
