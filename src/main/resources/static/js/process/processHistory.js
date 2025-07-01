9cb84a4c231b67bbb5c813064773db6b6e6dc410
Pangwoo <gwangwoo25@gmail.com>
2025-07-01 10:37:15 +0900

    feat: 공정 이력 프론트

--- src/main/resources/static/js/process/processHistory.js
+++ src/main/resources/static/js/process/processHistory.js
@@ -1,6 +1,108 @@
+// 날짜 비교 함수
+// return 1 when date1 is the latter than date2
+const compareDate = (date1, date2) => {
+    const dateObj1 = new Date(date1);
+    const dateObj2 = new Date(date2);
+
+    const date1Year = dateObj1.getFullYear();
+    const date1Month = dateObj1.getMonth() + 1;
+    const date1Day = dateObj1.getDate();
+    const date1Time = dateObj1.getTime();
+
+    const date2Year = dateObj2.getFullYear();
+    const date2Month = dateObj2.getMonth() + 1;
+    const date2Day = dateObj2.getDate();
+    const date2Time = dateObj2.getTime();
+
+    if(date1Year > date2Year) return 1;
+    if(date1Year < date2Year) return -1;
+    if(date1Month > date2Month) return 1;
+    if(date1Month < date2Month) return -1;
+    if(date1Day > date2Day) return 1;
+    if(date1Day < date2Day) return -1;
+    if(date1Time > date2Time) return 1;
+    if(date1Time < date2Time) return -1;
+    return 0;
+}
+
+// 그리드 데이터 날짜별로 정렬
+const sortByDate = (data) => {
+    return data.sort((a, b) => {
+        return compareDate(a.startTime, b.startTime);
+    });
+}
+
+// 그리드 입력 데이테 확인
+const nullCheck = (data) => {
+    return data.some(item => item === null || item === undefined);
+}
+
+// 공정간 작업시간 비교
+const checkTimeLine = (data) => {
+    data.forEach((row,i) => {
+        const currStartTime = data[i].startTime;
+        const currEndTime = data[i].endTime;
+
+        if(compareDate(currEndTime, currStartTime) !== 1) {
+            return false;
+        }
+
+        if(i > 0){
+            const prevEndTime = data[i - 1].endTime;
+            if(compareDate(currStartTime, prevEndTime) !== 1){
+                return false;
+            }
+        }
+    })
+    return true;
+}
+
+// 이전 공정과 산출물 비교
+const checkQty = (data) => {
+    data.forEach((item,i) => {
+        if(i > 0){
+            const currQty = data[i].outputQuantity;
+            const prevQty = data[i - 1].outputQuantity;
+
+            if(isNaN(currQty)){
+                return false;
+            }
+
+            if(isNaN(prevQty)){
+                return false;
+            }
+
+            if (Number(currQty) > Number(prevQty)) {
+                return false;
+            }
+        }
+    })
+    return true;
+}
+
+// 생산 계획 수량과 산출물 비교
+const checkPlanQty = (data) => {
+    data.forEach(item => {
+        const planQty = item.quantity;
+        const outQty = item.outputQuantity;
+        if(isNaN(planQty)){
+            alert("수량에는 숫자만 입력 가능합니다.")
+            return false;
+        }
+
+        if(isNaN(outQty)){
+            alert("수량에는 숫자만 입력 가능합니다.")
+            return false;
+        }
+
+        if(planQty < outQty) return false;
+    })
+    return true;
+}
+
 const init = () => {
     const btn= document.querySelector("button[name='workOrderBtn']")
-    console.log(btn);
+
     if(btn){
         btn.addEventListener("click", function (e) {
             e.preventDefault();
@@ -8,6 +110,47 @@ const init = () => {
             getWorkOrders();
         });
     }
+
+    const updateBtn = document.querySelector("button[name='updProcHist']")
+
+    if(updateBtn){
+        updateBtn.addEventListener("click", function (e) {
+            const isNotNull = !nullCheck(processHistoryGrid.getColumnValues("outputQuantity") && processHistoryGrid.getColumnValues("startTime")) && !nullCheck(processHistoryGrid.getColumnValues("endTime"))
+            const rawData = processHistoryGrid.getData();
+            const data = rawData.filter((row) => row.processStatusCode === 'STS001')
+
+            if(data.length === 0){
+                return;
+            }
+
+            if(!isNotNull){
+                alert("산출 수량, 공정 시작, 공정 종료는 반드시 입력해야합니다!")
+                return;
+            }
+
+            sortByDate(data)
+
+            if(!checkTimeLine(data)){
+                alert("공정 시간이 맞지 않습니다.");
+                return;
+            }
+
+            if(!checkQty(data)){
+                alert("산출 수량이 맞지 않습니다.");
+                return;
+            }
+
+            if(!checkPlanQty(data)){
+                alert("산출 수량이 작업수량보다 많습니다.")
+                return;
+            }
+
+            updateProcessHistory(data);
+            e.preventDefault();
+            e.stopPropagation();
+        });
+    }
+
     const workOrderGrid = initGrid(
         document.getElementById('workOrderGrid'),
         300,
@@ -62,6 +205,7 @@ const init = () => {
             },
         ]
     );
+
     getWorkOrders();
 
     const processHistoryGrid = initGrid(
@@ -83,12 +227,12 @@ const init = () => {
                 name: 'itemName',
                 align: 'center'
             },
-            {
-                header: '투입 수량',
-                name: 'inputQuantity',
-                align: 'center',
-                editor: 'text'
-            },
+            // {
+            //     header: '투입 수량',
+            //     name: 'inputQuantity',
+            //     align: 'center',
+            //     editor: 'text'
+            // },
             {
                 header: '산출 수량',
                 name: 'outputQuantity',
@@ -104,13 +248,27 @@ const init = () => {
                 header: '공정 시작',
                 name: 'startTime',
                 align: 'center',
-                editor: 'text'
+                editor: {
+                    type: 'datePicker',
+                    options: {
+                        format: 'yyyy-MM-dd HH:mm A',
+                        timepicker: true
+                    }
+                }
+
             },
             {
                 header: '공정 종료',
                 name: 'endTime',
                 align: 'center',
-                editor: 'text'
+                editor: {
+                    type: 'datePicker',
+                    options: {
+                        format: 'yyyy-MM-dd HH:mm A',
+                        timepicker: true
+                    }
+                }
+
             },
             {
                 header: '공정 상태',
@@ -168,13 +326,43 @@ const init = () => {
                     "Content-Type": "application/json"
                 },
             });
+            if (!res.ok) {
+                throw new Error(`서버 오류: ${res.status}`);
+            }
+            const data = await res.json();
+            processHistoryGrid.resetData(data.data);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    async function updateProcessHistory(data) {
+        const workOrderId = data[0].workOrderId;
+        const requestData = {
+            workOrderId: workOrderId,
+            processList: data.map(row => ({
+                processHistId:row.processHistoryId,
+                outputQty: row.outputQuantity,
+                startTime: new Date(row.startTime),
+                endTime: new Date(row.endTime),
+            }))
+        }
+        try {
+            const res = await fetch(`/api/process/history`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(requestData)
+            });
 
             if (!res.ok) {
                 throw new Error(`서버 오류: ${res.status}`);
             }
 
             const data = await res.json();
-            processHistoryGrid.resetData(data.data);
+            console.log(data.data)
+            getProcessHistory(workOrderId);
         } catch (e) {
             console.error(e);
         }
